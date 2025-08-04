9897aaccc89e7a646e04235e3385d4fceada1a29
BulueColour <monmon.9283@gmail.com>
2025-08-04 11:57:58 +0700

    First commit from AMAP, reached Article part

---
+++ app/src/main/java/com/example/a4_august/ui/theme/Article.kt
@@ -0,0 +1,55 @@
+package com.example.a4_august.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.a4_august.R
+
+@Composable
+fun Article() {
+    Column {
+        val image = painterResource(id = R.drawable.bg_compose_background)
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(painter = image, contentDescription = null)
+            Text(
+                text = "Jetpack Compose Tutorial",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp) //padding สามารถ custom ได้ ซึ่งมี start, end, top, bottom
+            )
+            Text(
+                text = stringResource(R.string.SecondParagraph), //เก็บข้อความอยู๋ใน res/values/strings.xml
+                fontSize = 16.sp,
+                textAlign = TextAlign.Justify, //จัด text ให้เท่ากันทั้งหมด
+            )
+            Text(
+                text = stringResource(R.string.ThirdParagraph),
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewArticle() {
+    
+}
