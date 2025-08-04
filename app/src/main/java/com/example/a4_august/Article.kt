b92b1b50205f5d34b9d4f3b23c718e3d4c8df282
BulueColour <monmon.9283@gmail.com>
2025-08-04 12:30:12 +0700

    Reached Tasks manager, only Compose exercise left

--- app/src/main/java/com/example/a4_august/Article.kt
+++ app/src/main/java/com/example/a4_august/Article.kt
@@ -1,12 +1,9 @@
-package com.example.a4_august.ui.theme
+package com.example.a4_august
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,7 +14,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.a4_august.R
 
 @Composable
 fun Article() {
@@ -36,8 +32,8 @@ fun Article() {
             )
             Text(
                 text = stringResource(R.string.SecondParagraph), //เก็บข้อความอยู๋ใน res/values/strings.xml
-                fontSize = 16.sp,
                 textAlign = TextAlign.Justify, //จัด text ให้เท่ากันทั้งหมด
+                modifier = Modifier.padding(16.dp)
             )
             Text(
                 text = stringResource(R.string.ThirdParagraph),
@@ -51,5 +47,5 @@ fun Article() {
 @Preview(showBackground = true)
 @Composable
 fun PreviewArticle() {
-    
+    Article()
 }
