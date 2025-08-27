8de5d4c66371e2c3658a30c2f3f4e3de2370c7c9
BulueColour <monmon.9283@gmail.com>
2025-08-27 23:08:42 +0700

    'This is exercises in class (including midterm exam)'

---
+++ app/src/main/java/com/example/a4_august/BulueColour.kt
@@ -0,0 +1,86 @@
+package com.example.a4_august
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.a4_august.ui.theme._4_AugustTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+
+class BulueColour : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) { //ทำงานตรงนี้
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _4_AugustTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {}
+            }
+        }
+    }
+}
+
+@Composable
+fun Card(message: String, from: String, modifier: Modifier = Modifier) { //การสร้างฟังก์ชันด้วย jet compose
+    Column(
+        verticalArrangement = Arrangement.Center, //ใช้เพื่อจัดให้อยู่ตรงกลาง
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp) //dp ใช้กับหน้าจอ sp ใช้กับ font
+    ) {
+        Text(
+            text = message,
+            fontSize = 80.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Left, //ใช้จัดตำแหน่งข้อความ (เช่น center ให้อยู่กลาง)
+            modifier = modifier // ใช้จัดการ layout การตกแต่งต่างๆ
+        )
+        Text(
+            text = from,
+            fontSize = 18.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun CardImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box(
+        modifier.fillMaxSize()
+    ) {
+        Image(painter = image, contentDescription = null)
+        Card(message = message, from = from)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CardPreview() {
+    Surface(
+        modifier = Modifier.background(color = MaterialTheme.colorScheme.background)
+    ) {
+        _4_AugustTheme {
+            GreetingImage("Happy birthday", "It's your day, enjoy!")
+        }
+    }
+}
